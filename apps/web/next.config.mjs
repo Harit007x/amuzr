961fc959c95a5e5079f08837f45505756a9173d4
Harit007x <harit.ptl.business@gmail.com>
2024-10-16 14:42:26 +0530

    removed the env from next config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -30,9 +30,6 @@ const nextConfig = {
     ],
   },
   transpilePackages: ['@repo/db', '@prisma/client', '@repo/recoil'],
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,  // Ensure Prisma's env() can access this
-  },
 };
 
 export default nextConfig;
